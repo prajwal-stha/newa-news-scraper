aa4f5aa485f25198736af0250c4db975ae96f6a6
navarajhazesoft <navaraj.pokharel@hazesoft.co>
2024-01-11 13:12:26 +0545

    nepal mandal scrapper updated

--- news/spiders/nepalmandal.py
+++ news/spiders/nepalmandal.py
@@ -8,13 +8,15 @@ class NepalMandal(scrapy.Spider):
     name = 'nepalmandal'
     start_urls = [f"http://www.nepalmandal.com/cat/59/{i}.html" for i in range(0, 25)]
     custom_settings = dict(LOG_ENABLED=False)
+
     def __init__(self):
         self.outfile = open("nepal_mandal/मू बुखँ.csv", "w", newline="", encoding="utf-8-sig")
         self.writer = csv.writer(self.outfile)
-        self.writer.writerow(['headline', 'date', 'final_news','url'])
+        self.writer.writerow(['headline', 'date', 'final_news', 'url'])
 
     def parse(self, response, **kwargs):
         for links in response.xpath("//div[@class='post']//a[@class='nepali']/@href").extract():
+            print(f"http://www.nepalmandal.com/{links}")
             yield scrapy.Request(
                 url=f"http://www.nepalmandal.com/{links}",
                 callback=self.get_full_news)
@@ -25,5 +27,4 @@ class NepalMandal(scrapy.Spider):
         # for news in response.xpath("//div[@class='entry']//p/text()").extract():
         cleaned_list = [x.strip() for x in response.xpath("//div[@class='entry']//p/text()").extract() if x.strip()]
         final_news = " ".join(cleaned_list)
-        self.writer.writerow([headline, posted_on,final_news,response.url ])
-
+        self.writer.writerow([headline, posted_on, final_news, response.url])
